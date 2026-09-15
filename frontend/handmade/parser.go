6f3ce59ccc4c47b9b5eb07e2eb058c87c579a872
100495713 <nicola.cindea0103@gmail.com>
2026-09-15 00:15:43 +0000

    Implement const declarations and break parsing

--- frontend/handmade/parser.go
+++ frontend/handmade/parser.go
@@ -240,9 +240,10 @@ func (parser *Parser) Statement() StmtNode {
 func (parser *Parser) DeclStatement() *DeclStmtNode {
 	var decl DeclNode
 	token := parser.token
-	if parser.matchIf(TtKwConst) {
+	switch parser.token.Type {
+	case TtKwConst:
 		decl = parser.ConstDecl()
-	} else {
+	case TtKwVar:
 		decl = parser.VarDecl()
 	}
 	return &DeclStmtNode{Tok: token, Decl: decl}
@@ -377,17 +378,58 @@ func (parser *Parser) BasicLiteral() ExprNode {
 ////////////////// You implement the methods below (add methods as needed) ////////////////////
 
 func (parser *Parser) ConstDecl() DeclNode {
-	// TO DO ...
-	return nil
+	var (
+		spec  SpecNode
+		specs []SpecNode
+	)
+
+	token := parser.token
+	parser.match(TtKwConst)
+
+	if parser.matchIf(TtLParen) {
+		for parser.token.Type == TtIdentifier {
+			spec = parser.ConstSpec()
+			specs = append(specs, spec)
+			parser.match(TtSemicolon)
+		}
+		parser.match(TtRParen)
+	} else {
+		spec = parser.ConstSpec()
+		specs = append(specs, spec)
+	}
+
+	return &GenDeclNode{Tok: token, Specs: specs}
 }
 
 func (parser *Parser) ConstSpec() *ValueSpecNode {
-	// TO DO ...
-	return nil
+	var exprs []ExprNode
+
+	token := parser.token
+	idents := parser.Identifiers()
+	typeExpr := parser.TypeName()
+
+	parser.match(TtOpAssign)
+
+	expr := parser.BasicLiteral()
+	exprs = append(exprs, expr)
+
+	for parser.matchIf(TtComma) {
+		expr = parser.BasicLiteral()
+		exprs = append(exprs, expr)
+	}
+
+	return &ValueSpecNode{
+		Token:  token,
+		Names:  idents,
+		Type:   typeExpr,
+		Values: exprs,
+	}
 }
 
 func (parser *Parser) BreakStatement() StmtNode {
-	// TO DO ...
+	token := parser.token
+	parser.match(TtKwBreak)
+	return &BranchStmtNode{Tok: token}// TO DO ...
 	return nil
 }
 
